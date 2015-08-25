9260ab17fcf374b8311d6f93e1540c9b23b89693
k3nn7 <lukasz.lalik@gmail.com>
2015-08-25 10:41:25 +0200

    Basic activities

---
+++ app/src/main/java/net/lalik/shipbattles/BattleCenterActivity.java
@@ -0,0 +1,48 @@
+package net.lalik.shipbattles;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class BattleCenterActivity extends Activity {
+    private ProgressDialog findOpponentProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_center);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_battle_center, menu);
+        return true;
+    }
+
+    public void newBattleClicked(View view) {
+        findOpponentProgress = ProgressDialog.show(
+                this,
+                getText(R.string.app_name),
+                getText(R.string.we_are_finding_opponnent_for_you),
+                true
+        );
+        new FindOpponentTask().execute();
+    }
+
+    private class FindOpponentTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(2000);
+                findOpponentProgress.dismiss();
+            } catch (InterruptedException e) {
+            }
+            return null;
+        }
+    }
+}
--- app/src/main/java/net/lalik/shipbattles/EntryActivity.java
+++ app/src/main/java/net/lalik/shipbattles/EntryActivity.java
@@ -1,11 +1,16 @@
 package net.lalik.shipbattles;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class EntryActivity extends Activity {
+    private ProgressDialog registerProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +18,37 @@ public class EntryActivity extends Activity {
         setContentView(R.layout.activity_entry);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_entry, menu);
-        return true;
+    public void signInClicked(View view) {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void registerClicked(View view) {
+        registerProgress = ProgressDialog.show(
+                this,
+                getText(R.string.app_name),
+                getText(R.string.registering_account),
+                true
+        );
+        new RegisterAccountTask().execute();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void enterBattleCenter() {
+        Intent intent = new Intent(this, BattleCenterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private class RegisterAccountTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(2000);
+                registerProgress.dismiss();
+                enterBattleCenter();
+            } catch (InterruptedException e) {
+            }
+            return null;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
