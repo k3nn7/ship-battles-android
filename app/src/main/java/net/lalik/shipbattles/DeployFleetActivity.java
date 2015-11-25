9a329ac31c5cbb4ba30d69c703a5a8993955591b
Łukasz Lalik <lukasz.lalik@gmail.com>
2015-11-25 22:22:04 +0100

    Use drag'n'drop battlefield view in DeployFleetActivity

--- app/src/main/java/net/lalik/shipbattles/DeployFleetActivity.java
+++ app/src/main/java/net/lalik/shipbattles/DeployFleetActivity.java
@@ -1,16 +1,11 @@
 package net.lalik.shipbattles;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-import net.lalik.shipbattles.fragments.CoordinatesOrientationDialogFragment;
 import net.lalik.shipbattles.sdk2.ShipBattles;
 import net.lalik.shipbattles.sdk2.entity.Account;
 import net.lalik.shipbattles.sdk2.entity.Battle;
@@ -18,8 +13,7 @@ import net.lalik.shipbattles.sdk2.entity.MyBattlefield;
 import net.lalik.shipbattles.sdk2.entity.ShipClass;
 import net.lalik.shipbattles.sdk2.value.Coordinate;
 import net.lalik.shipbattles.sdk2.value.Orientation;
-import net.lalik.shipbattles.views.MyBattlefieldView;
-import net.lalik.shipbattles.views.ShipsInventoryListAdapter;
+import net.lalik.shipbattles.views.DeployBattlefieldView;
 
 import java.util.Map;
 
@@ -27,75 +21,18 @@ public class DeployFleetActivity extends Activity {
     private Account account;
     private Battle battle;
     private MyBattlefield battlefield;
-    private MyBattlefieldView myBattlefieldView;
-    private TextView shipsInventoryText;
+    private DeployBattlefieldView myBattlefieldView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deploy_fleet);
-        myBattlefieldView = (MyBattlefieldView)findViewById(R.id.battlefield);
-        shipsInventoryText = (TextView)findViewById(R.id.ships_inventory_text);
+        myBattlefieldView = (DeployBattlefieldView) findViewById(R.id.battlefield);
 
         Intent intent = getIntent();
-        new GetBattlesTask().execute(intent.getStringExtra(BattleCenterActivity.AUTH_TOKEN));
-    }
-
-    public void deployShipClicked(View view) {
-        getSelectShipClassDialog().show();
-    }
-
-    public void clearBattlefieldClicked(View view) {
-        myBattlefieldView.clear();
-    }
-
-    public void commitBattlefieldClicked(View view) {
-        new ReadyForBattleTask().execute();
-    }
-
-    private void updateInventoryDisplay() {
-        shipsInventoryText.setText("");
-        ShipClass[] availableShipClasses = battlefield.getAvailableShipClasses();
-        for (ShipClass shipClass : availableShipClasses) {
-            shipsInventoryText.append(String.format(
-                    "%s (%d)",
-                    shipClass.getName(),
-                    battlefield.shipsCountInInventory(shipClass)
-            ));
-        }
-    }
-
-    private AlertDialog.Builder getSelectShipClassDialog() {
-        final ShipsInventoryListAdapter adapter = new ShipsInventoryListAdapter(
-                this,
-                battlefield.getAvailableShipClasses()
-        );
-        return new AlertDialog.Builder(this)
-                .setAdapter(adapter, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        getSelectCoordinatesDialog(
-                                adapter.getItem(which)
-                        ).show(getFragmentManager(), "CoordinatesOrientationDialogFragment");
-                    }
-                });
-    }
-
-    private CoordinatesOrientationDialogFragment getSelectCoordinatesDialog(final ShipClass shipClass) {
-        CoordinatesOrientationDialogFragment dialog = new CoordinatesOrientationDialogFragment();
-        dialog.setListener(new CoordinatesOrientationDialogFragment.CoordinatesDialogListener() {
-            @Override
-            public void onDeployClicked(Coordinate coordinate, Orientation orientation) {
-                new DeployShipTask().execute(
-                        account,
-                        shipClass,
-                        coordinate,
-                        orientation
-                );
-            }
-        });
-        return dialog;
+        String sessionToken = "172bc83648184fe9b296321cd1184900";
+//        sessionToken = intent.getStringExtra(BattleCenterActivity.AUTH_TOKEN);
+        new GetBattlesTask().execute(sessionToken);
     }
 
     private class GetBattlesTask extends AsyncTask<String, Void, net.lalik.shipbattles.sdk2.entity.Battle> {
@@ -122,25 +59,20 @@ public class DeployFleetActivity extends Activity {
         @Override
         protected void onPostExecute(net.lalik.shipbattles.sdk2.entity.Battle b) {
             battle = b;
-            myBattlefieldView.setShipClasses(shipClasses);
-
             battlefield = battle.getMyBattlefield();
-
             myBattlefieldView.setBattlefield(battlefield);
-
-            updateInventoryDisplay();
-
             registerProgress.dismiss();
+            myBattlefieldView.invalidate();
         }
     }
 
     private class DeployShipTask extends AsyncTask<Object, Void, Void> {
         @Override
         protected Void doInBackground(Object... params) {
-            Account account = (Account)params[0];
-            ShipClass shipClass = (ShipClass)params[1];
-            Coordinate coordinate = (Coordinate)params[2];
-            Orientation orientation = (Orientation)params[3];
+            Account account = (Account) params[0];
+            ShipClass shipClass = (ShipClass) params[1];
+            Coordinate coordinate = (Coordinate) params[2];
+            Orientation orientation = (Orientation) params[3];
 
             battlefield = ShipBattles.getInstance().deployShip(
                     account,
@@ -153,8 +85,6 @@ public class DeployFleetActivity extends Activity {
         @Override
         protected void onPostExecute(Void aVoid) {
             myBattlefieldView.setBattlefield(battlefield);
-            myBattlefieldView.updateShots();
-            updateInventoryDisplay();
         }
     }
 
